ef955eb6fe0b24ac872466f3d1288ba05478593c
Carlos López Garcés <carlos.lopez.garces@gmail.com>
2023-06-03 11:17:43 -0600

    Add frustum and occlusion culling compute shader for meshes

--- source/chapter6/shaders/culling.glsl
+++ source/chapter6/shaders/culling.glsl
@@ -1,10 +1,11 @@
-
+// Compute shader for frustum and occlusion culling of meshes.
+// See meshlet.glsl for task shader that does the same for meshlets.
 
 #if defined(COMPUTE_GPU_CULLING)
 
-
-layout(set = MATERIAL_SET, binding = 1) writeonly buffer VisibleMeshInstances
-{
+layout(set = MATERIAL_SET, binding = 1) writeonly buffer VisibleMeshInstances {
+	// When a mesh passes frustum and occlusion culling testing, a draw
+	// command for it is written here.
 	MeshDrawCommand draw_commands[];
 };
 
@@ -37,93 +38,124 @@ layout(set = MATERIAL_SET, binding = 13) buffer EarlyVisibleMeshCount
 	uint early_late_flag;
 };
 
-// 2D Polyhedral Bounds of a Clipped, Perspective-Projected 3D Sphere. Michael Mara, Morgan McGuire. 2013
+// 2D Polyhedral Bounds of a Clipped, Perspective-Projected 3D Sphere. Michael Mara, Morgan McGuire. 2013.
+// See https://jcgt.org/published/0002/02/05/.
+//
+// Returns an axis-aligned bounding box in screen space of the input world-space bounding sphere.
+//
+// C is the center of the bounding sphere and r is its radius.
+// P00 and P11 are the first 2 diagonal values of the view-projection matrix.
 bool project_sphere(vec3 C, float r, float znear, float P00, float P11, out vec4 aabb) {
-	if (-C.z - r < znear)
+	if (-C.z - r < znear) {
+		// The bounding sphere is behind the near plane.
 		return false;
+	}
 
+	// Compute minimum and maximum points on the x axis (in the xz plane) in world space.
 	vec2 cx = vec2(C.x, -C.z);
 	vec2 vx = vec2(sqrt(dot(cx, cx) - r * r), r);
 	vec2 minx = mat2(vx.x, vx.y, -vx.y, vx.x) * cx;
 	vec2 maxx = mat2(vx.x, -vx.y, vx.y, vx.x) * cx;
 
+	// Compute minimum and maximum points on the y axis (in the yz plane) in world space.
 	vec2 cy = -C.yz;
 	vec2 vy = vec2(sqrt(dot(cy, cy) - r * r), r);
 	vec2 miny = mat2(vy.x, vy.y, -vy.y, vy.x) * cy;
 	vec2 maxy = mat2(vy.x, -vy.y, vy.y, vy.x) * cy;
 
+	// Build an axis-aligned bounding box for the sphere using these points, transforming the aabb
+	// to perspective-projected screen space along the way.
 	aabb = vec4(minx.x / minx.y * P00, miny.x / miny.y * P11, maxx.x / maxx.y * P00, maxy.x / maxy.y * P11);
-	aabb = aabb.xwzy * vec4(0.5f, -0.5f, 0.5f, -0.5f) + vec4(0.5f); // clip space -> uv space
+
+	// Transform aabb from screen space (where coordinates are in the range [-1, 1])
+	// to UV space (where coordinates are in the range [0, 1]).
+	//
+	// y coordinates are multiplied by a negative number because screen space has a
+	// bottom-left origin, while UV space has a top-left origin.
+	aabb = aabb.xwzy * vec4(0.5f, -0.5f, 0.5f, -0.5f) + vec4(0.5f);
 
 	return true;
 }
 
-
 layout (local_size_x = 64, local_size_y = 1, local_size_z = 1) in;
-void main() {
 
+void main() {
+	// TODO: there's no mention of mesh instances in the book.
 	uint mesh_instance_index = gl_GlobalInvocationID.x;
 	uint count = total_count;
 	if ( late_flag == 1 ) {
 		count = early_opaque_mesh_culled_count;
 	}
 
-	// TODO:
 	if ( mesh_instance_index == 0 && late_flag == 0 ) {
 		debug_draw_line( vec3(-1,-1,-1), vec3(1,1,1), vec4(0,0,1,1), vec4(1,0,0,1));
-		//debug_draw_box(vec3(-1,-1,-1), vec3(1,1,1), vec4(0,0,1,1));
+		// debug_draw_box(vec3(-1,-1,-1), vec3(1,1,1), vec4(0,0,1,1));
 	}
 
 	if (mesh_instance_index < count) {
 		if (late_flag == 1) {
 			mesh_instance_index = draw_late_commands[mesh_instance_index].drawId;
 		}
-		uint mesh_draw_index = mesh_instance_draws[mesh_instance_index].mesh_draw_index;
 
+		// A MeshDraw contains the mesh's textures, colors, and meshlet info. See mesh.h.
+		uint mesh_draw_index = mesh_instance_draws[mesh_instance_index].mesh_draw_index;
 		MeshDraw mesh_draw = mesh_draws[mesh_draw_index];
 
+		// The meshes bounding sphere is used to do frustum culling.
 		vec4 bounding_sphere = mesh_bounds[mesh_draw_index];
-		mat4 model = mesh_instance_draws[mesh_instance_index].model;
 
 		// Transform bounding sphere to view space.
+		mat4 model = mesh_instance_draws[mesh_instance_index].model;
 		vec4 world_bounding_center = model * vec4(bounding_sphere.xyz, 1);
 		vec4 view_bounding_center = freeze_occlusion_camera == 0 ? world_to_camera * world_bounding_center : world_to_camera_debug * world_bounding_center;
 
-    	float scale = length( model[0] );
-    	float radius = bounding_sphere.w * scale * 1.1;	// Artificially inflate bounding sphere.
+    	float scale = length(model[0]);
+		// Inflate bounding sphere. Scale is obtained from the model matrix?
+    	float radius = bounding_sphere.w * scale * 1.1;
 
+		// Check bounding sphere against the 6 planes of the camera frustum.
     	bool frustum_visible = true;
-	    for ( uint i = 0; i < 6; ++i ) {
-	        frustum_visible = frustum_visible && (dot( frustum_planes[i], view_bounding_center) > -radius);
+	    for (uint i = 0; i < 6; ++i) {
+	        frustum_visible = frustum_visible && (dot(frustum_planes[i], view_bounding_center) > -radius);
 	    }
 
+		// TODO: what does frustum_cull_meshes do?
 	    frustum_visible = frustum_visible || (frustum_cull_meshes == 0);
 
+		// If the mesh passes frustum culling, do occlusion culling.
 	    bool occlusion_visible = true;
-	    if ( frustum_visible ) {
+	    if (frustum_visible) {
 	    	vec4 aabb;
-	    	if ( project_sphere(view_bounding_center.xyz, radius, z_near, projection_00, projection_11, aabb ) ) {
-    			// TODO: improve
+			// project_sphere returns false when the bounding sphere is completely
+			// behind the near plane.
+	    	if (project_sphere(view_bounding_center.xyz, radius, z_near, projection_00 , projection_11, aabb)) {
+				// aabb (produced by project_sphere()) is a UV space projection of the 
+				// axis-aligned bounding box of the mesh's bounding sphere.
+
+				// Determine the mip level of the depth pyramid that should be used.
     			ivec2 depth_pyramid_size = textureSize(global_textures[nonuniformEXT(depth_pyramid_texture_index)], 0);
+				// aabb.z - aabb.x yields a number between 0 and 1, so width is a fraction
+				// of the width of the top level texture of the depth pyramid. Same goes for height.
 	    		float width = (aabb.z - aabb.x) * depth_pyramid_size.x;
 				float height = (aabb.w - aabb.y) * depth_pyramid_size.y;
-
 				float level = floor(log2(max(width, height)));
 
-				// Sampler is set up to do max reduction, so this computes the minimum depth of a 2x2 texel quad
+				// TODO: ?
+				// Sampler is set up to do max reduction, so this computes the minimum depth of a 2x2 texel quad.
 				vec2 uv = (aabb.xy + aabb.zw) * 0.5;
             	uv.y = 1 - uv.y;
 				
+				// Look up the current depth at this UV coordinate.
 				float depth = textureLod(global_textures[nonuniformEXT(depth_pyramid_texture_index)], uv, level).r;
-				// Sample also 4 corners
+				// Sample also 4 corners.
             	depth = max(depth, textureLod(global_textures[nonuniformEXT(depth_pyramid_texture_index)], vec2(aabb.x, 1.0f - aabb.y), level).r);
             	depth = max(depth, textureLod(global_textures[nonuniformEXT(depth_pyramid_texture_index)], vec2(aabb.z, 1.0f - aabb.w), level).r);
             	depth = max(depth, textureLod(global_textures[nonuniformEXT(depth_pyramid_texture_index)], vec2(aabb.x, 1.0f - aabb.w), level).r);
             	depth = max(depth, textureLod(global_textures[nonuniformEXT(depth_pyramid_texture_index)], vec2(aabb.z, 1.0f - aabb.y), level).r);
 
+				// Compute the closest depth of the bounding sphere.
 				vec3 dir = normalize(eye.xyz - world_bounding_center.xyz);
     			vec4 sceen_space_center_last = late_flag == 0 ? previous_view_projection * vec4(world_bounding_center.xyz + dir * radius, 1.0) : view_projection * vec4(world_bounding_center.xyz + dir * radius, 1.0);
-
 				float depth_sphere = sceen_space_center_last.z / sceen_space_center_last.w;
 
 				occlusion_visible = (depth_sphere <= depth);
@@ -133,11 +165,14 @@ void main() {
 		occlusion_visible = occlusion_visible || (occlusion_cull_meshes == 0);
 
 	    uint flags = mesh_draw.flags;
-	    if ( frustum_visible && occlusion_visible ) {
-	    	// Add opaque draws
-			if ( (flags & (DrawFlags_AlphaMask | DrawFlags_Transparent)) == 0 ) {
-				uint draw_index = atomicAdd( opaque_mesh_visible_count, 1 );
+	    if (frustum_visible && occlusion_visible) {
+	    	// Mesh passed frustum and occlusion testing. Record draw command for it.
+			// The draw_commands buffer records up to total_count opaque draws followed
+			// by transparent draws.
 
+			if ((flags & (DrawFlags_AlphaMask | DrawFlags_Transparent)) == 0) {
+				// Opaque draws. Record it right after the last opaque draw.
+				uint draw_index = atomicAdd(opaque_mesh_visible_count, 1);
 				draw_commands[draw_index].drawId = mesh_instance_index;
 				draw_commands[draw_index].indexCount = 0;
 				draw_commands[draw_index].instanceCount = 1;
@@ -148,9 +183,9 @@ void main() {
 				draw_commands[draw_index].firstTask = mesh_draw.meshlet_offset / 32;
 			}
 			else {
-				// Transparent draws are written after total_count commands in the same buffer.
+				// Transparent draw. Note that its command is not written in the next
+				// available slot, but after total_count commands in the same buffer.
 				uint draw_index = atomicAdd( transparent_mesh_visible_count, 1 ) + total_count;
-
 				draw_commands[draw_index].drawId = mesh_instance_index;
 				draw_commands[draw_index].indexCount = 0;
 				draw_commands[draw_index].instanceCount = 1;
@@ -161,6 +196,7 @@ void main() {
 				draw_commands[draw_index].firstTask = mesh_draw.meshlet_offset / 32;
 			}
 	    } else if ( late_flag == 0 ) {
+			// TODO: ?
 			// Add culled object for re-test
 			if ( (flags & (DrawFlags_AlphaMask | DrawFlags_Transparent)) == 0 ) {
 				uint draw_index = atomicAdd( opaque_mesh_culled_count, 1 );
@@ -171,6 +207,7 @@ void main() {
 			}
 		}
 
+		// Commands recorded in draw_commands will be used to draw meshlets of visible meshes.
 	}
 }
 
